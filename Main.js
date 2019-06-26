fc553ff535ca62d470d157541dccdbda3ed938e0
mtsealove <mtsealove0927@gmail.com>
2019-06-26 16:56:53 +0900

    이전 운행기록 읽기 운행정보 검색 쿼리 수정

--- Main.js
+++ Main.js
@@ -7,11 +7,18 @@ var server = http.createServer(function (req, res) {
 }).listen(3000, function () {
     console.log('모바일 클라이언트 실행중');
 });
+var Err = {
+    Result: 'Err'
+};
+var ResultOK = {
+    Result: 'OK'
+};
 
 // 소켓 서버를 생성한다.
 var io = socketio.listen(server);
 io.sockets.on('connection', function (socket) {
     console.log('Socket ID : ' + socket.id + ', Connect');
+
     //로그인 
     socket.on('Login', function (data) {
         console.log('email : ' + data.email);
@@ -19,7 +26,8 @@ io.sockets.on('connection', function (socket) {
 
         var ErrMsg = {
             Name: 'none',
-            ID: 'none'
+            ID: 'none',
+            cat: 3
         };
 
         var result = mysql.Login(data.email, data.password);
@@ -29,7 +37,7 @@ io.sockets.on('connection', function (socket) {
         else socket.emit('serverMessage', ErrMsg);
     });
 
-    //운행정보 리스트
+    //운행정보 리스트 검색
     socket.on('GetRunInfo', function (data) {
         var key = data.key;
         var Company = data.Company;
@@ -43,7 +51,7 @@ io.sockets.on('connection', function (socket) {
             socket.emit('RunInfo', 'Err');
     });
 
-    //운행정보 각각
+    //운행정보 각각 검색
     socket.on('GetRunInfoByID', function (data) {
         var ID = data.RunInfoID;
         console.log("ID: " + ID);
@@ -54,19 +62,46 @@ io.sockets.on('connection', function (socket) {
         else
             socket.emit('RunInfoByID', 'Err');
     });
-    //우리회사 기사님
+    //우리회사 기사님 검색
     socket.on('GetFreeDriver', function (data) {
-        console.log("기사님 호출");ㅎ
+        console.log("기사님 호출");
         var CompanyID = data.CompanyID;
         var date = data.date;
-        var cat=data.cat;
-        var key=data.key;
-        var result = mysql.GetFreeDrivers(CompanyID, date,cat, key);
+        var cat = data.cat;
+        var key = data.key;
+        var result = mysql.GetFreeDrivers(CompanyID, date, cat, key);
         if (result != null)
             socket.emit('FreeDriver', result);
         else
             socket.emit('FreeDriver', 'Err');
     });
+    //RunInfoEach 삽입
+    socket.on('CreateRunInfoEach', function (data) {
+        console.log("운행정보 삽입");
+        var RunInfoID = data.RunInfoID;
+        var DriverID = data.DriverID;
+        var RunDate=data.RunDate;
+
+        var result = mysql.CreateRunInfoEach(RunInfoID, DriverID, RunDate);
+        if (result) { //데이터 삽입 성공 시
+            socket.emit('RunInfoEach', ResultOK);
+        } else {
+            socket.emit('RunInfoEach', Err);
+        }
+    });
+
+    //기사별 이전 운행기록
+    socket.on('GetPrevRunInfo', function(data){
+        var DriverID=data.DriverID;
+        var key=data.key;
+        var result=mysql.GetPrevRunInfo(DriverID, key);
+        if(result){
+            socket.emit('PrevRunInfo', result);
+        } else {
+            socket.emit('PrevRunInfo', Err);
+        }
+    });
+ 
 });
 
 var express = require('express');
--- SQL.js
+++ SQL.js
@@ -1,6 +1,6 @@
-var mysqlSync=require('sync-mysql');
+var mysqlSync = require('sync-mysql');
 //연결 정보
-var connection=new mysqlSync({
+var connection = new mysqlSync({
     host: 'localhost',
     user: 'ueue',
     password: 'Fucker0916!',
@@ -9,46 +9,99 @@ var connection=new mysqlSync({
 });
 
 //로그인
-exports.Login=function(ID, password) {
+exports.Login = function (ID, password) {
     //기사 ID로 먼저 로그인
-    var query=`select ID, Name, cat from BusDrivers where ID='${ID}' and password='${password}'`;
-    var result=connection.query(query)[0];
+    var query = `select ID, Name, cat from BusDrivers where ID='${ID}' and password='${password}'`;
+    var result = connection.query(query)[0];
     //기사 ID 로그인 실패 시
-    if(result==null) {  //회사 회원으로 로그인
-        query=`select ID, Name, cat from BusCompany where ID='${ID}' and password='${password}'`;
-        result=connection.query(query)[0];
+    if (result == null) {  //회사 회원으로 로그인
+        query = `select ID, Name, cat from BusCompany where ID='${ID}' and password='${password}'`;
+        result = connection.query(query)[0];
     }
     return result;
 }
 //운행정보 전체 얻기
-exports.GetRunInfo=function(Company, ID, key) {
-    var query='';
-    if(Company) {
-        query=`select * from RunInfo where Company='${Company}'`;
-    } else if(ID) {
-        query=`select * from RunInfoEach RIE, RunInfo RI where RIE.RunInfoID=RI.ID and RIE.DriverID='${ID}'`
-    }
+exports.GetRunInfo = function (Company, ID, key) {
+    var dateFormat = require('dateformat');
+        var date = new Date();
+        var dateStr = dateFormat(date, 'yyyy-mm-dd');
+    var query = '';
     
-    if(key!=null)
-        query+=` and startAddr like '%${key}%' or endAddr like '%${key}%'`;
-    var result=connection.query(query);
+    if (Company) {
+        //오늘이 계약 기간 내에 속한 것만 출력
+        query = `select * from RunInfo where Company='${Company}'
+        and '${dateStr}'<=ContractEnd
+        and '${dateStr}'>=ContractStart`;
+    } else if (ID) {
+        //오늘 이후의 것만 출력    
+        query = `select * from RunInfoEach RIE, RunInfo RI 
+        where RIE.RunInfoID=RI.ID 
+        and RIE.DriverID='${ID}'
+        and RIE.RunDate>='${dateStr}'`;
+    }
+
+    if (key != null)
+        query += ` and startAddr like '%${key}%' or endAddr like '%${key}%'`;
+    var result = connection.query(query);
     console.log(result);
     return result;
 }
 
 //운행정보 상세
-exports.GetRunInfoDetail=function(ID) {
-    var query=`select * from RunInfo where ID=${ID}`;
-    var result=connection.query(query)[0];
+exports.GetRunInfoDetail = function (ID) {
+    var query = `select * from RunInfo where ID=${ID}`;
+    var result = connection.query(query)[0];
     return result;
 };
 
 //특정 날짜에 운행이 없는 우리회사의 기사님
-exports.GetFreeDrivers=function(CompanyID, date, cat, key) {
-    var query=`select distinct * from BusDrivers where CompanyID='${CompanyID}' and ID in(select DriverID from RunInfoEach where not RunDate='${date}')`;
-    if(cat&&key){   //검색어 존재
-        query+=`and ${cat}='${key}'`;
+exports.GetFreeDrivers = function (CompanyID, date, cat, key) {
+    var query = `select distinct * from BusDrivers where CompanyID='${CompanyID}' and ID not in(select DriverID from RunInfoEach where RunDate='${date}')`;
+    if (cat && key) {   //검색어 존재
+        query += `and ${cat}='${key}'`;
     }
+    var result = connection.query(query);
+    return result;
+}
+
+//운행정보에 데이터 삽입
+exports.CreateRunInfoEach = function (RunInfoID, DriverID, RunDate) {
+    var query;
+    var existQeury = `select count(*) as count
+    from RunInfoEach where RunInfoID=${RunInfoID}
+    and RunDate='${RunDate}'`;
+    var exist = connection.query(existQeury)[0].count;   //기존 데이터 존재 여부
+    if (exist == 0)
+        query = `insert into RunInfoEach(RunInfoID, DriverID, RunDate) values(${RunInfoID}, '${DriverID}', '${RunDate}')`;
+    else
+        query = `update RunInfoEach set DriverID='${DriverID}'
+        where RunInfoID=${RunInfoID} and RunDate='${RunDate}'`;
+
+    var result = connection.query(query);
+    return result;
+};
+
+//각 기사별 이전 운행기록 확인
+exports.GetPrevRunInfo = function (DriverID, key) {
+    var dateFormat = require('dateformat');
+    var date = new Date();
+    var dateStr = dateFormat(date, 'yyyy-mm-dd');
+    query = `select * from RunInfoEach RIE, RunInfo RI 
+        where RIE.RunInfoID=RI.ID 
+        and RIE.DriverID='${DriverID}'
+        and RIE.RunDate<'${dateStr}'`;
+
+    if(key){
+        query+=` and (
+            RI.startAddr like'%${key}%'
+            or RI.startName like'%${key}%'
+            or RI.endName like '%${key}%'
+            or RI.endAddr like '%${key}%'
+            or RI.wayloadCats like '%${key}%'
+            or RI.wayloadAddrs like '%${key}%'
+            or RI.wayloadNames like '%${key}%')`
+    }
+    query+='order by RIE.RunDate desc';
     var result=connection.query(query);
     return result;
 }
